800dd9182c10a2fae900699e2acd2d24038c5283
Ingmar Jager <info@ingmarjager.nl>
2015-10-28 10:02:00 +0100

    Add cpu param to flash script to make it less specific. Telnet waits for OpenOCD to fully start before issueing commands.

--- flash.py
+++ flash.py
@@ -6,23 +6,36 @@ import os
 
 os.chdir(os.path.dirname(os.path.realpath(__file__)))
 
-parser = argparse.ArgumentParser(description="Connects to a running OpenOCD server via telnet to flash a target with a given binary file to a given memory address")
-parser.add_argument("binary",  help="The binary hex file to flash to the device")
-parser.add_argument("address",  help="The memory address to flash the binary hex file to")
-parser.add_argument("-b2", "--binary2",  help="The binary hex file to flash to the device")
-parser.add_argument("-a2", "--address2",  help="The memory address to flash the binary hex file to")
-parser.add_argument("-ip","--ip", type=str, help="The OpenOCD ip address to connect to", default="localhost")
-parser.add_argument("-p","--port", type=int, help="The OpenOCD port to connect to", default=4444)
+parser = argparse.ArgumentParser(description="Connects to a running \
+        OpenOCD server via telnet to flash a target with a given \
+        binary file to a given memory address")
+parser.add_argument("binary",  help="The binary hex file to flash to \
+        the device")
+parser.add_argument("address",  help="The memory address to flash the \
+        binary hex file to")
+parser.add_argument("cpu",  help="The cpu to flash, corresponding to \
+        the openocd config file [cpu].cfg")
+parser.add_argument("-b2", "--binary2",  help="The binary hex file to \
+        flash to the device")
+parser.add_argument("-a2", "--address2",  help="The memory address to \
+        flash the binary hex file to")
+parser.add_argument("-ip","--ip", type=str, help="The OpenOCD ip \
+        address to connect to", default="localhost")
+parser.add_argument("-p","--port", type=int, help="The OpenOCD port to\
+        connect to", default=4444)
 args = parser.parse_args()
 
 cmd_start = 'init; reset halt'
-cmd_flash = [('flash write_image erase '+args.binary+' '+args.address+';','verify_image ' + args.binary +' '+args.address+';')]
+cmd_flash = [('flash write_image erase '+args.binary+' '+args.address+';',
+    'verify_image ' + args.binary +' '+args.address+';')]
 
 cmd_done = 'reset run;'
 cmd_shutdown = 'shutdown'
 
 if not args.binary2 is None:
-    cmd_flash.append(('flash write_image erase '+args.binary2+' '+args.address2+';',    'verify_image ' + args.binary2 +' '+args.address2+';'))
+    cmd_flash.append(('flash write_image erase '+args.binary2+' \
+            '+args.address2+';',    'verify_image ' + args.binary2 +' \
+            '+args.address2+';'))
 
 # detect running openocd server
 openocdFound = False
@@ -35,18 +48,15 @@ for line in os.popen("ps xa"):
 
 if not openocdFound:
     def run_openocd():
-        if(int(args.address,16) != 0):
-                cpu = 'lpc4337_swd'
-        else:
-                cpu = 'lpc11uxx'
-        os.popen('openocd -f ' + cpu + '.cfg' )
+        os.popen('openocd -f ' + args.cpu + '.cfg' )
 
     thread = Thread(target=run_openocd)
     thread.deamon = True
     thread.start()
-    time.sleep(4)
-
+    time.sleep(0.1)
 tn = Telnet(args.ip, args.port)
+r = tn.expect(['Open On-Chip Debugger'])
+
 tn.write(cmd_start + '\n')
 for cmd in cmd_flash:
     done = False
@@ -62,23 +72,9 @@ for cmd in cmd_flash:
             break
     print('Flashing done after %d tries\n' % tries)
 tn.write(cmd_done + '\n')
+time.sleep(0.01)
 if not openocdFound:
     print('shutdown openocd...')
     tn.write(cmd_shutdown + '\n')
-    tn.write(cmd_shutdown + '\n')
 time.sleep(0.5)
 tn.sock.close()
-
-print('done')
-#else:
-#	if(int(args.address,16) != 0):
-#		cpu = 'lpc4337_swd'
-#	else:
-#		cpu = 'lpc11uxx'
-#
-	#cmdStr = ''.join(cmdList)	
-	#print("Programming with openOCD:")
-	#os.popen('openocd -f ' + cpu + '.cfg -c "'+cmdStr+'"')
-
-
-
